36e5bd360c5427eb757fd897d91b8c46691d78e6
Joaquin999 <32464518+Joaquin999@users.noreply.github.com>
2018-04-20 11:03:54 +0100

    Add files via upload

---
+++ Prácticas/Tercer Trimestre/Actividad 1 Js/codigo.js
@@ -0,0 +1,8 @@
+function myFunction() {
+    var nombre;
+    nombre = prompt("Nombre completo");
+    //Introduzco la variable nombre y luego declaro que la variable será igual al resultado del promt
+    alert("Bienvenido  " +nombre+"!"); 
+    //Luego uso alert para decir que el navegador muestre la variable nombre además de "Bienvenido...!" 
+    alert("Esto funciona loco");
+}
